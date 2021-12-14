3d8d268475a71248b0e70274f9e2266d7aa23c69
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-12-14 17:37:54 +0900

    build: 백엔드 프로젝트 생성
    - git ignore 설정해야함

---
+++ be/qnaboard/src/main/java/scra/qnaboard/QnaboardApplication.java
@@ -0,0 +1,13 @@
+package scra.qnaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaboardApplication.class, args);
+	}
+
+}
---
+++ be/qnaboard/src/test/java/scra/qnaboard/QnaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package scra.qnaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
