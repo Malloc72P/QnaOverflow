9208ef4360b8f970827b1b11638d9d8519ddbed2
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-12-16 11:45:50 +0900

    build: docker-compose 설정 추가

--- be/qnaboard/src/main/java/scra/qnaboard/QnaboardApplication.java
+++ be/qnaboard/src/main/java/scra/qnaboard/QnaboardApplication.java
@@ -1,15 +1,25 @@
 package scra.qnaboard;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import javax.annotation.PostConstruct;
+
 @Slf4j
+@SpringBootApplication
+@RequiredArgsConstructor
 public class QnaboardApplication {
+
+    private final TestRepository testRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(QnaboardApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        testRepository.save(new TestEntity("asdf"));
+    }
 }
