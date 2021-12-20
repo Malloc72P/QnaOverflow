1dbd8831db8e290662fa2024dcc2efac624155dd
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-12-20 18:36:18 +0900

    chore: 태그 엔티티 구현

--- be/qnaboard/src/main/java/scra/qnaboard/domain/entity/Tag.java
+++ be/qnaboard/src/main/java/scra/qnaboard/domain/entity/Tag.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 /**
  * 회원에 대한 Member 엔티티.
@@ -22,4 +23,29 @@ public class Tag extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id")
     private Member author;
+
+    private String name;
+
+    private String color;
+
+    public Tag(Member author, String name, String color) {
+        this.author = author;
+        this.name = name;
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(getId(), tag.getId()) &&
+                Objects.equals(getName(), tag.getName()) &&
+                Objects.equals(getColor(), tag.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor());
+    }
 }
