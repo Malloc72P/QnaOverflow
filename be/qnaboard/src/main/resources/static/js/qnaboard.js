fd7d6ac1b08b8eb0ed6e13fc7baedd9f8fdaf8bb
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-12-27 16:35:17 +0900

    feat: 답변 보기 및 댓글작성 폼 생성
    - 아직 기능은 못붙임
    - CSS 다듬음
    - 페이지를 프래그먼트로 잘게 분리함

--- be/qnaboard/src/main/resources/static/js/qnaboard.js
+++ be/qnaboard/src/main/resources/static/js/qnaboard.js
@@ -1,11 +1,15 @@
 let elements = document.querySelectorAll(".btn");
 
+/**
+ * 부트스트랩 버튼의 애니메이션을 고치기 위한 함수. 포커스를 대상 엘리먼트에 부여하고, 150초 후에 풀어버린다.
+ *
+ * @param element 대상 엘리먼트
+ */
 const handleBtnClickEffect = (element) => {
     element.focus();
     setTimeout(() => {
         console.log("fired")
         element.blur();
-        element.classList.remove("hasactive");
     }, 150);
 };
 
