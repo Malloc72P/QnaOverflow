2eeafe5d42e37dc13c28424caff2e83451a4411c
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2022-01-14 18:31:58 +0900

    chore: 개행추가

--- be/qnaboard/src/test/java/scra/qnaboard/domain/repository/question/QuestionSearchDetailRepositoryTest.java
+++ be/qnaboard/src/test/java/scra/qnaboard/domain/repository/question/QuestionSearchDetailRepositoryTest.java
@@ -24,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 class QuestionSearchDetailRepositoryTest {
+
     @Autowired
     private QuestionSearchDetailRepository questionSearchDetailRepository;
 
