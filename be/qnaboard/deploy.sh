cbc3bd4ae06379210cde0f7b5471804b6b04d66b
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2022-01-21 03:41:48 -1000

    deploy: 배포스크립트 버그 수정
    - 불필요한 커맨드 제거
    - 잘못된 경로에 압축풀던 문제 수정

--- be/qnaboard/deploy.sh
+++ be/qnaboard/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #현재 실행중인 서버 중지
-fuser -k 8080/tcp
+sudo fuser -k 8080/tcp
 #로그파일의 이름 생성
 logname="$(date +%y-%m-%d-%H-%M).log"
 #젠킨스에서 빌드해서 업로드한 파일 압축 해제
-unzip /home/ec2-user/release/deploy-be.zip
-#로그파일 생성
-touch /home/ec2-user/log$logname
+sudo unzip /home/ec2-user/release/deploy-be.zip -d /home/ec2-user/release/
 #서버 실행
-nohup java -jar \
+sudo nohup java -jar \
         -Dspring.profiles.active=dev \
         /home/ec2-user/release/qnaboard-0.0.1-SNAPSHOT.jar \
         > /home/ec2-user/log/$logname 2>&1 &
