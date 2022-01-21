b5c3b1c0f996cf7a09671d3d4d77d2897a5bc774
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2022-01-21 03:49:52 -1000

    deploy: 배포스크립트에서 sudo 제거

--- be/qnaboard/deploy.sh
+++ be/qnaboard/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #현재 실행중인 서버 중지
-sudo fuser -k 8080/tcp
+fuser -k 8080/tcp
 #로그파일의 이름 생성
 logname="$(date +%y-%m-%d-%H-%M).log"
 #젠킨스에서 빌드해서 업로드한 파일 압축 해제
-sudo unzip /home/ec2-user/release/deploy-be.zip -d /home/ec2-user/release/
+unzip /home/ec2-user/release/deploy-be.zip -d /home/ec2-user/release/
 #서버 실행
-sudo nohup java -jar \
+nohup java -jar \
         -Dspring.profiles.active=dev \
         /home/ec2-user/release/qnaboard-0.0.1-SNAPSHOT.jar \
         > /home/ec2-user/log/$logname 2>&1 &
