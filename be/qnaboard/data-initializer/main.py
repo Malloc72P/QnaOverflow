ba81413539fa8bf7b81e1a07c1c188194f3126e4
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2022-01-26 15:33:36 +0900

    feat: 테스트 데이터 입력을 위한 파이썬 스크립트 작성함

--- be/qnaboard/data-initializer/main.py
+++ be/qnaboard/data-initializer/main.py
@@ -1,71 +1,27 @@
-# 데이터 초기화를 위한 스크립트
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import datetime
-import random
-import csv
+from query_generator import QueryGenerator
 
 
 class DataInitializer:
+    __member_count = 10
+    __question_count = 10000
+    __answer_count = 20000
+    __tag_count = 10
+    __comment_count = 40000
     # data.sql파일
     __file_data_sql = None
-    # 랜덤날짜의 범위(시작, 종료일)
-    __start_date = datetime.date(2020, 1, 1)
-    __end_date = datetime.date(2020, 2, 1)
-    # 랜덤 값이 저장되어 있는 리스트
-    __random_names = []
-    __random_sentences = []
+    # 쿼리 생성기
+    __query_generator = None
 
-    # 초기화 함수
+    def __init__(self):
+        self.__query_generator = QueryGenerator()
+        self.__file_data_sql = open("../src/main/resources/data.sql", "w")
+
     def initialize(self):
-        self.__prepare_files()
+        query = self.__query_generator.create_query(self.__member_count, self.__question_count, self.__answer_count, self.__tag_count, self.__comment_count)
+        self.__write_string(query)
 
-    # 필요한 파일을 생성하거나 불러옴
-    def __prepare_files(self):
-        self.__file_data_sql = open("data.sql", "wb")
-        self.__random_names = self.__file_to_list("names.csv")
-        self.__random_sentences = self.__file_to_list("sentences.csv")
-
-    # 파일명을 파라미터로 받아서 파일을 열고 csv reader를 만들어서 라인단위로 읽은 다음 리스트에 담아서 반환함
-    @staticmethod
-    def __file_to_list(file_name):
-        file_object = open(file_name, "r")
-        csv_reader = csv.reader(file_object, delimiter="\t")
-        random_list = []
-        for i, line in enumerate(csv_reader):
-            random_list.append(line)
-        return random_list
-
-    # 랜덤날짜를 생성해서 반환함
-    def __random_date(self):
-        time_between_dates = self.__end_date - self.__start_date
-        days_between_dates = time_between_dates.days
-        random_number_of_days = random.randrange(days_between_dates)
-        random_date = self.__start_date + datetime.timedelta(random_number_of_days)
-        return random_date
-
-    # 랜덤이름을 생성해서 반환함
-    def __random_name(self):
-        random_name = random.choice(self.__random_names)
-        return self.__without_bracket(random_name)
-
-    # 랜덤제목을 생성해서 반환함
-    def __random_title(self):
-        random_title = random.choice(self.__random_sentences)
-        return self.__without_bracket(random_title)
-
-    # 랜덤컨텐츠를 생성해서 반환함
-    def __random_content(self):
-        content = ""
-        for i in range(10):
-            random_sentence = random.choice(self.__random_sentences)
-            content += self.__without_bracket(random_sentence) + " "
-        return content
-
-    @staticmethod
-    def __without_bracket(string):
-        return str(string)[2:-2]
+    def __write_string(self, string):
+        self.__file_data_sql.write(string + "\n")
 
 
 # 데이터 이니셜라이져 객체 생성 후 데이터 초기화
---
+++ be/qnaboard/data-initializer/query_generator.py
@@ -0,0 +1,145 @@
+from random_data_supplier import RandomDataSupplier
+import random
+
+
+class QueryGenerator:
+    # id 생성기
+    __current_id = 1
+    # query head
+    __insert_into_members = "insert into members (members_id, created_date, last_modified_date, deleted, email, nickname, role) values"
+    __insert_into_tag = "insert into tag (tag_id, created_date, last_modified_date, author_id, deleted, description, name) values"
+    __insert_into_post = "insert into post (post_id, created_date, last_modified_date, author_id, content, deleted, score, title, view_count, post_type, question_id) values"
+    __insert_into_question_tag = "insert into question_tag (created_date, last_modified_date, question_id, tag_id) values"
+    __insert_into_comment = "insert into comment (comment_id, created_date, last_modified_date, author_id, content, deleted, parent_comment_id, parent_post_id) values"
+    __insert_into_vote = "insert into vote (member_id, post_id, vote_type) values"
+    # 랜덤값 생성기
+    __random_generator = None
+    # id 리스트
+    __member_ids = []
+    __post_ids = []
+    __question_ids = []
+    __answer_ids = []
+    __tag_ids = []
+
+    # 생성자
+    def __init__(self):
+        self.__random_generator = RandomDataSupplier()
+
+    def create_query(self, member_count, question_count, answer_count, tag_count, comment_count):
+        string_list = [self.create_members(member_count), self.create_question(question_count),
+                       self.create_answer(answer_count), self.create_tag(tag_count), self.create_question_tag(),
+                       self.create_comment(comment_count)]
+        return "\n".join(string_list)
+
+    # 멤버 생성 쿼리를 반환함
+    def create_members(self, number_of_rows):
+        return self.__create_query(number_of_rows, self.__values_members, self.__insert_into_members)
+
+    # 질문글 생성 쿼리를 반환함
+    def create_question(self, number_of_rows):
+        return self.__create_query(number_of_rows, self.__values_question, self.__insert_into_post)
+
+    # 답변글 생성 쿼리를 반환함
+    def create_answer(self, number_of_rows):
+        return self.__create_query(number_of_rows, self.__values_answer, self.__insert_into_post)
+
+    # 태그 생성 쿼리를 반환함
+    def create_tag(self, number_of_rows):
+        return self.__create_query(number_of_rows, self.__values_tag, self.__insert_into_tag)
+
+    # 질문태그 생성 쿼리를 반환함
+    def create_question_tag(self):
+        values = []
+        # 모든 질문글을 순회함
+        for question_id in self.__question_ids:
+            tag_ids = set()
+            # 태그 5개를 무작위로 선택함(중복허용x)
+            for i in range(5):
+                tag_ids.add(random.choice(self.__tag_ids))
+            for tag_id in tag_ids:
+                values.append(self.__values_question_tag(question_id, tag_id))
+        joined_values = ",\n".join(values)
+        return "{} \n{};".format(self.__insert_into_question_tag, joined_values)
+
+    # 댓글 생성 쿼리를 반환함
+    def create_comment(self, number_of_rows):
+        return self.__create_query(number_of_rows, self.__values_comment, self.__insert_into_comment)
+
+    # 쿼리를 생성해서 반환함
+    @staticmethod
+    def __create_query(number_of_rows, values_func, query_head):
+        values = []
+        for i in range(number_of_rows):
+            value = values_func()
+            values.append(value)
+        joined_values = ",\n".join(values)
+        return "{} \n{};".format(query_head, joined_values)
+
+    # 멤버생성을 위한 values 부분을 하나 만들어서 반환함. 추가로 발급받은 아이디를 리스트에 넣어줌
+    def __values_members(self):
+        given_id = self.__get_id()
+        self.__member_ids.append(given_id)
+        given_created_date = self.__random_generator.random_date()
+        given_name = self.__random_generator.random_name()
+        return "({}, '{}', null, false, 'no-email', '{}', 'USER')".format(given_id, given_created_date, given_name)
+
+    # 질문생성을 위한 values 부분을 하나 만들어서 반환함. 추가로 발급받은 아이디를 리스트에 넣어줌
+    def __values_question(self):
+        given_id = self.__get_id()
+        self.__question_ids.append(given_id)
+        self.__post_ids.append(given_id)
+        given_created_date = self.__random_generator.random_date()
+        given_title = self.__random_generator.random_title()
+        given_content = self.__random_generator.random_content()
+        given_member_id = random.choice(self.__member_ids)
+        return "({}, '{}', null, {}, '{}', false, 0, '{}', 0, 'Question', null)".format(given_id, given_created_date,
+                                                                                        given_member_id,
+                                                                                        given_content, given_title)
+
+    # 답변생성을 위한 values 부분을 하나 만들어서 반환함. 추가로 발급받은 아이디를 리스트에 넣어줌
+    def __values_answer(self):
+        given_id = self.__get_id()
+        self.__answer_ids.append(given_id)
+        self.__post_ids.append(given_id)
+        given_created_date = self.__random_generator.random_date()
+        given_title = self.__random_generator.random_title()
+        given_content = self.__random_generator.random_content()
+        given_member_id = random.choice(self.__member_ids)
+        given_question_id = random.choice(self.__question_ids)
+        return "({}, '{}', null, {}, '{}', false, 0, '{}', 0, 'Answer', {})".format(given_id, given_created_date,
+                                                                                         given_member_id, given_content,
+                                                                                         given_title, given_question_id)
+
+    # 태그생성을 위한 values 부분을 하나 만들어서 반환함. 추가로 발급받은 아이디를 리스트에 넣어줌
+    def __values_tag(self):
+        given_id = self.__get_id()
+        self.__tag_ids.append(given_id)
+        given_created_date = self.__random_generator.random_date()
+        given_tag_name = self.__random_generator.random_tag()
+        given_tag_desc = self.__random_generator.random_tag_desc()
+        given_member_id = random.choice(self.__member_ids)
+        return "({}, '{}', null, {}, false, '{}', '{}')".format(given_id, given_created_date, given_member_id,
+                                                                given_tag_desc,
+                                                                given_tag_name)
+
+    # 질문태그생성을 위한 values 부분을 하나 만들어서 반환함. 추가로 발급받은 아이디를 리스트에 넣어줌
+    def __values_question_tag(self, given_question_id, given_tag_id):
+        given_created_date = self.__random_generator.random_date()
+        return "('{}', null, {}, {})".format(given_created_date, given_question_id, given_tag_id)
+
+    # 댓글생성을 위한 values 부분을 하나 만들어서 반환함. 추가로 발급받은 아이디를 리스트에 넣어줌
+    def __values_comment(self):
+        given_id = self.__get_id()
+        given_created_date = self.__random_generator.random_date()
+        given_post_id = random.choice(self.__post_ids)
+        given_member_id = random.choice(self.__member_ids)
+        given_content = self.__random_generator.random_comment_content()
+        return "({}, '{}', null, {}, '{}', false, null, {})".format(given_id, given_created_date, given_member_id,
+                                                                    given_content,
+                                                                    given_post_id)
+
+    # 아이디를 생성해서 반환함
+    def __get_id(self):
+        return_value = self.__current_id
+        self.__current_id += 1
+        return return_value
